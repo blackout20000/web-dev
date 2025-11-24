27c7da793d58a2e38a21e40a94497b3282f459fd
blackout2000 <blackout2000@naver.com>
2025-11-24 21:24:55 +0900

    added app.js exercise.js operations.js updated index.html

---
+++ Web_Dev_100_coding/Section 11/exercise.js
@@ -0,0 +1,58 @@
+// This is how a comment is added to JavaScript
+// Comments are not executed - they are just there to provide extra
+// information to you or other developers
+
+// Exercise Time!
+
+// 1) Create three new variables:
+//    - A variable that stores the name of an online course of your choice
+//    - A variable that stores the price of that course
+//    - A variable that stores the three main goals that you have, when taking this course
+
+let lectureName = "Web Development - The Complete Guide";
+let lecturePrice = 39;
+let lectureGoal = ["Learn Web Development", "Becom a Web Developer", "Have Fun!"];
+
+
+// 2) Output ("alert") the three variable values
+
+alert(lectureName);
+alert(lecturePrice);
+alert(lectureGoal);
+
+// 3) Try "grouping" the three variables together and still output their values thereafter
+
+let onlineLecture = { 
+    name: "Web Development",
+    price: 39,
+    goals: ["Front End", "Back End", "Full Stack"]
+    
+ };
+
+ alert(onlineLecture.name)
+ alert(onlineLecture.price);
+ alert(onlineLecture.goals);
+
+
+// 4) Also output the second element in your "main goals" variable
+
+alert(onlineLecture.goals[1]);
+
+// 5) Add a custom command that does the following:
+//    - Use your "main goals" variable and access an element by its identifier
+//    - The concrete identifier value should be dynamic / flexible 
+//      (i.e. the command can be executed for different identifier)
+//    - The "main goals" variable should also be dynamic: The command should work 
+//      with ANY list of values
+//    - The custom command should provide the accessed value (i.e. the list element)
+
+function getListItem(array, arrayIndex) {
+    let arrayElement = array[arrayIndex];
+    return arrayElement;
+}
+
+
+// 6) Execute your custom command from (5) and output ("alert") the result
+
+let firstGoal = getListItem(onlineLecture.goals, 0);
+alert(firstGoal);
---
+++ Web_Dev_100_coding/Section 11/operations.js
@@ -0,0 +1,18 @@
+// Math Operations
+
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4);
+console.log(10 % 3);
+
+console.log(10 + 3 - 5 * 10);
+console.log((10 + 3 - 5) * 10);
+
+console.log('Max' + ' ' + 'Schwarzmuller');
+let userName = 'Max';
+console.log(userName.length);
+
+let hobbies = ['Sports', 'Cooking'];
+console.log(hobbies.length);
